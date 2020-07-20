ce258f666675db4369e673d8b30b41e614d579f3
gicsaw0 <gicsaw0@gmail.com>
2020-07-20 23:48:25 +0900

    condo

--- bin/ConDo.sh
+++ bin/ConDo.sh
@@ -18,9 +18,9 @@ $ConDobin/run_jackhmmer.sh $target $nprocessor
 $ConDobin/run_ccmpred.sh $target $nprocessor
 $ConDobin/gen_features.sh $target $nprocessor
 $ConDobin/feature.py $target 
-$ConDobin/pred_ss.py $weight_dir
-$ConDobin/pred_sa.py $weight_dir
-$ConDobin/pred_dom.py $weight_dir
+$ConDo/pred_ss.py $weight_dir
+$ConDo/pred_sa.py $weight_dir
+$ConDo/pred_dom.py $weight_dir
 
 $ConDobin/gen_results.py $target
 
--- bin/gen_features.sh
+++ bin/gen_features.sh
@@ -13,10 +13,6 @@ uniprot20=$DBPATH/hh-suite/uniprot20/uniprot20_2016_02
 hhpath=/lustre/protein/programs/hh-suite
 hhbindir=$hhpath/bin
 log="gen_features.log"
-psipred=""
-sann=""
-#dir of database.rsa, database.vec
-NNDB_HOME=$sann/nndb
 
 $hhbindir/hhblits -i $target.fa -d $uniprot20 -oa3m $target.a3m -cpu $ncpu 2>&1 | tee -a $log
 
@@ -28,12 +24,4 @@ if [ ! -e $target.chk ]; then
     echo "check it"
     exit
 fi
-
-$psipred/runpsipred_single $target
-
-# sann2 (using new database)
-$sann/bin/mkchk2.py $target  | tee -a $log
-$sann/bin/sann -i $target -np $ncpu
-ln -s $target.a3 $target.sa2
-
 
