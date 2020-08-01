77bd4efdd5b6b72ac119f7ae9b217ef114476d21
gicsaw0 <gicsaw0@gmail.com>
2020-08-01 22:41:49 +0900

    draw figure

--- bin/gen_results.py
+++ bin/gen_results.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Secondary Structure
 COIL = ['S', 'T', ' ', '_']  # ' ' == '_'
@@ -21,7 +22,7 @@ SA3_str = 'BIE'
 SA3_dict = {'B': 0, 'I': 1, 'E': 2}
 
 
-def gen_results_ss(target, seq, pred_dir):
+def read_results_ss(target, seq, pred_dir):
 
     Ypred_ss8_file = pred_dir + '/Ypred_ss8.npy'
     Ypred_ss8 = np.load(Ypred_ss8_file)
@@ -63,8 +64,10 @@ def gen_results_ss(target, seq, pred_dir):
         fp_ss3.write(line_out)
     fp_ss3.close()
 
+    return Ypred_ss8, Ypred_ss3
 
-def gen_results_sa(target, seq, pred_dir):
+
+def read_results_sa(target, seq, pred_dir):
 
     Ypred_sa3_file = pred_dir + '/Ypred_sa3.npy'
     Ypred_rsa_file = pred_dir + '/Ypred_rsa.npy'
@@ -88,8 +91,10 @@ def gen_results_sa(target, seq, pred_dir):
         fp_sa.write(line_out)
     fp_sa.close()
 
+    return Ypred_sa3, Ypred_rsa
 
-def gen_results_dom(target, seq, pred_dir, conf_cut=1.3, NC=40):
+
+def read_results_dom(target, seq, pred_dir, conf_cut=1.3, NC=40):
 
     Ypred_dom_file = pred_dir + '/Ypred_dom.npy'
 
@@ -141,7 +146,7 @@ def gen_results_dom(target, seq, pred_dir, conf_cut=1.3, NC=40):
     if len(bd2) == 0:
         print(target+' is single domain')
         line_out = 'predicted boundary: None '
-        return
+        return Ypred_dom, boundary2, bd2
 
     arg = np.array(bd2)[:, 0].argsort()
     for j in range(0, len(bd2)):
@@ -157,13 +162,13 @@ def gen_results_dom(target, seq, pred_dir, conf_cut=1.3, NC=40):
         line_out = 'predicted boundary: '
         print(line_out)
         print(line_db)
-        return
+        return Ypred_dom, boundary2, bd2
 
     print('#' + target + ' is multi domain')
     line_out = '#predicted boundary: residue_index score '
     print(line_out)
     print(line_db)
-    return
+    return Ypred_dom, boundary2, bd2
 
 
 def read_fasta(file_name):
@@ -183,6 +188,166 @@ def read_fasta(file_name):
     return seq
 
 
+def write_output(out_file, seq, Ypred_ss8, Ypred_ss3, Ypred_sa3, boundary2, line_len):
+    num_res = len(seq)
+
+    line_ss8 = ''
+    line_ss3 = ''
+    line_sa3 = ''
+    line_db = ''
+
+    be = []
+    bs = []
+    for b in boundary2:
+        be += [b[0]]
+        bs += [b[1]]
+
+    for i in range(num_res):
+        j_ss8 = Ypred_ss8[i].argmax()
+        ss8 = SS8_str[j_ss8]
+        line_ss8 += ss8
+        j_ss3 = Ypred_ss3[i].argmax()
+        ss3 = SS3_str[j_ss3]
+        line_ss3 += ss3
+
+        j_sa3 = Ypred_sa3[i].argmax()
+        sa3 = SA3_str[j_sa3]
+        line_sa3 += sa3
+
+        if i in be:
+            line_db += '>'
+        elif i in bs:
+            line_db += '<'
+        else:
+            line_db += '-'
+
+    num_lines = int(np.ceil(num_res/line_len))
+    fp_out = open(out_file, 'w')
+    for k in range(num_lines):
+        ini = k * line_len
+        fin = (k+1) * line_len
+        if fin > num_res:
+            fin = num_res
+        ll = fin-ini
+        blank = ' '*(ll-5)
+        line_out = '          %-4d%s%d\n' % (ini+1, blank, fin)
+        line_out += 'query   : %s\n' % seq[ini:fin]
+        line_out += 'ss8     : %s\n' % line_ss8[ini:fin]
+        line_out += 'ss3     : %s\n' % line_ss3[ini:fin]
+        line_out += 'sa3     : %s\n' % line_sa3[ini:fin]
+        line_out += 'boundary: %s\n' % line_db[ini:fin]
+        line_out += '\n'
+
+        fp_out.write(line_out)
+    fp_out.close()
+
+
+def draw_pas(pas_file, pas_fig, num_res):
+    fp = open(pas_file)
+    lines = fp.readlines()
+    fp.close()
+    x_list = []
+    y_list = []
+    z_list = []
+    for i, line in enumerate(lines):
+        lis = line.strip().split()
+        x_list += [int(lis[0])]
+        y_list += [int(lis[1])]
+        z_list += [float(lis[2])]
+
+    x = np.array(x_list).reshape(num_res, num_res)
+    y = np.array(y_list).reshape(num_res, num_res)
+    z = np.array(z_list).reshape(num_res, num_res)
+
+    plt.figure(figsize=(6, 6))
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=16, labelsize=16)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    plt.rc('legend', fontsize=12)
+    plt.rc('figure', titlesize=24)
+
+    plt.xlabel('Sequence')
+    plt.ylabel('Sequence')
+    plt.title('PAS')
+    plt.pcolormesh(x, y, z)
+    plt.tight_layout()
+    plt.savefig(pas_fig, dpi=300)
+
+
+def draw_ccm(ccm_file, ccm_fig, num_res):
+    fp = open(ccm_file)
+    lines = fp.readlines()
+    fp.close()
+    x_list = []
+    y_list = []
+    z_list = []
+    for i, line in enumerate(lines):
+        lis = line.strip().split()
+        x = int(lis[0])
+        y = int(lis[1])
+        z = float(lis[2])
+        if z <= 0.00000001:
+            continue
+        x_list += [x]
+        y_list += [y]
+        z_list += [z]
+
+    x = np.array(x_list)
+    y = np.array(y_list)
+    z = np.array(z_list)
+
+    plt.figure(figsize=(6, 6))
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=16, labelsize=16)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    plt.rc('legend', fontsize=12)
+    plt.rc('figure', titlesize=24)
+
+    plt.xlabel('Sequence')
+    plt.ylabel('Sequence')
+    plt.title('CCMpred')
+    plt.scatter(x, y, s=z, c=z, marker='o')
+    plt.tight_layout()
+#    plt.show()
+    plt.savefig(ccm_fig, dpi=300)
+
+
+def draw_com(com_file, com_fig, num_res):
+
+    fp = open(com_file)
+    lines = fp.readlines()
+    fp.close()
+    x_list = []
+    y_list = []
+    for i, line in enumerate(lines):
+        lis = line.strip().split()
+        x = int(lis[0])
+        y = float(lis[1])
+        x_list += [x]
+        y_list += [y]
+
+    x = np.array(x_list)
+    y = np.array(y_list)
+
+    plt.figure(figsize=(6, 4))
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=16, labelsize=16)
+    plt.rc('xtick', labelsize=12)
+    plt.rc('ytick', labelsize=12)
+    plt.rc('legend', fontsize=12)
+    plt.rc('figure', titlesize=24)
+
+    plt.xlabel('Sequence')
+    plt.ylabel('Modularity')
+    plt.title('')
+    plt.plot(x, y, '-')
+    plt.tight_layout()
+#    plt.show()
+    plt.savefig(com_fig, dpi=300)
+
+
 def main():
 
     if len(sys.argv) < 2:
@@ -193,11 +358,30 @@ def main():
     fasta_file = './' + target+'.fasta'
     seq = read_fasta(fasta_file)
 
+    num_res = len(seq)
+    pas_file = './' + target + '_PAS4.txt'
+    pas_fig = './' + target + '_PAS4.png'
+    draw_pas(pas_file, pas_fig, num_res)
+
+    ccm_file = './result_ccm.txt'
+    ccm_fig = './result_ccm.png'
+    draw_ccm(ccm_file, ccm_fig, num_res)
+
+    com_file = './community_ccm.txt'
+    com_fig = './community_ccm.png'
+    draw_com(com_file, com_fig, num_res)
+
     pred_dir = '.'
-    gen_results_ss(target, seq, pred_dir)
-    gen_results_sa(target, seq, pred_dir)
+    Ypred_ss8, Ypred_ss3 = read_results_ss(target, seq, pred_dir)
+    Ypred_sa3, Ypred_rsa = read_results_sa(target, seq, pred_dir)
     conf_cut = 1.3
-    gen_results_dom(target, seq, pred_dir, conf_cut)
+    Ypred_dom, boundary2, bd2 = read_results_dom(
+        target, seq, pred_dir, conf_cut)
+
+    line_len = 60
+    out_file = './' + target + '_result.txt'
+    write_output(out_file, seq, Ypred_ss8, Ypred_ss3,
+                 Ypred_sa3, boundary2, line_len)
 
 
 if __name__ == '__main__':
